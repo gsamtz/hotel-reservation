1ba48a919b7d6ec4a69d8d9ba6b5eb43fa3d4c38
Samuel Gutierrez <samg@sg.lan>
2023-09-27 08:33:49 -0500

    Add CRUD reservation service

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Reservation } from '../models/reservation';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationService {
 
-  constructor() { }
+  private reservations: Reservation[] = [];
+
+  getAllReservations(): Reservation[] {
+    return this.reservations;
+  }
+
+  getSingleReservation(id: string): Reservation | undefined {
+    return this.reservations.find(element => element.id === id);
+  }
+  
+  addReservation(reservation: Reservation): void {
+    this.reservations.push(reservation);
+  }
+
+  deleteReservation(id: string): void {
+    let targetIndex = this.reservations.findIndex(element => element.id === id);
+    this.reservations.splice(targetIndex, 1);
+  }
+
+  updateReservation(updatedReservation: Reservation): void {
+    let targetIndex = this.reservations.findIndex(element => element.id === updatedReservation.id);
+    this.reservations[targetIndex] = updatedReservation;
+  }
 }
